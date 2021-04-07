94dd96069358f2f4ca09417eb448a7361c9d47f8
Sawyerinvi <55842725+Sawyerinvi@users.noreply.github.com>
2021-04-07 17:07:24 +0600

    07.04.21

--- Tower defence/Assets/Scripts/WaveSpawner.cs
+++ Tower defence/Assets/Scripts/WaveSpawner.cs
@@ -1,23 +1,41 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 
 public class WaveSpawner : MonoBehaviour
 {
     public Transform enemyPrefab;
+    public Transform spawnPoint;
     public float timeBetweenWaves = 10f;
+    public Text waveCountdownText;
+
     private float countDown = 2f;
+    private int waveIndex = 0;
 
     private void Update()
     {
         if(countDown <= 0f)
         {
-            SpawnWave();
+            StartCoroutine(SpawnWave());
             countDown = timeBetweenWaves;
         }
         countDown -= Time.deltaTime;
+        waveCountdownText.text = Mathf.Floor(countDown).ToString();
     }
 
-    void SpawnWave()
+    IEnumerator SpawnWave()
     {
-        Debug.Log("Wave incoming");
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.35f);
+        }
+        
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
     }
 }
