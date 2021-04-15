c3a3560e68ffe9e5203b2403325d3d2236b82a9d
Sawyerinvi <55842725+Sawyerinvi@users.noreply.github.com>
2021-04-15 17:21:04 +0600

    15.04.21

---
+++ Tower defence/Assets/Scripts/Shop.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public void PurchaseStandartTurret()
+    {
+        Debug.Log("Standart Turret Purchaase");
+    }
+
+    public void PurchaseRocketTurret()
+    {
+        Debug.Log("Standart Rocket Purchaase");
+    }
+}
